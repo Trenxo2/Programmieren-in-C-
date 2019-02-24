5a7ed16109ce60ceb7a907c9f148adf0b482c107
Trenxo2 <33938808+Trenxo2@users.noreply.github.com>
2019-02-24 12:50:48 +0100

    Äh.. Meine Erste Klasse

---
+++ Program.cs
@@ -0,0 +1,150 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        // Mit dieser Funktion kann der Entwickler bestimmen wie oft eine Neue Zeile ausgegeben werden soll. Hierfür muss einfach der Wert in der for-schleife von "4" in eine beliebige Zahl geändert werden.
+        static void nz()
+        {
+
+            for (int x = 1; x < 4; x++)
+            {
+                Console.WriteLine("\n");
+            }
+
+        }
+        // Mit dieser Funktion kann der Nutzer selber bestimmen wie oft eine Neue Zeile ausgegeben werden soll. 
+        static void cstm_nz(int y)
+        {
+
+            for (int x = 1; x < y; x++)
+            {
+                Console.WriteLine("\n");
+            }
+
+        }
+
+        static void Main(string[] args)
+        {
+
+            int[] eingabe = new int[5];
+
+            double[] input = new double[20], erg = new double[10];
+
+            // Console.WriteLine gibt den Text aus der in der Klammer steht, jedoch ist es nicht möglich in der Zeile eine Eingabe tätigen zu können
+
+            Console.WriteLine("\t\tSelbst geschriebenes C# Lernprogramm\n");
+
+            // Console.Write gibt einen Text aus und ermöglicht, nicht so wie .WriteLine, dass man noch in der Zeile eine Eingabe tätigen kann.
+
+            Console.Write("Geben Sie eine Zahl ein: ");
+
+            eingabe[0] = Convert.ToInt32(Console.ReadLine());
+
+            // Typische if-schleife. Sollte man aus vorherigen Sprachen wie C oder C++ kennen.
+
+            if (eingabe[0] > 100 || eingabe[0] < 0)
+            {
+
+                Console.WriteLine("Dies ist eine Ungültige Eingabe!");
+
+                return;
+
+            }
+            else
+            {
+
+                Console.WriteLine("\nIhre Eingabe lautete: {0}\n", eingabe[0]);
+
+                Console.WriteLine("\t\tWarnung: \n\t\tDezimalzahlen könnten das Programm zum Absturz bringen!\n");
+                Console.WriteLine("\nMoechten Sie das Programm beenden?\n");
+                Console.WriteLine("\n0.Ja(fürs beenden des Programmes)\n1.Nein(fürs weiterführen des Programmes)");
+
+                Console.Write("\nGeben Sie eine der oben genannten Werte ein: ");
+
+                eingabe[1] = Convert.ToInt32(Console.ReadLine());
+
+                // Typische Switch-Case anweisung. Sollte man wie die if-schleife schon aus vorheringen Sprachen wie C oder C++ kennen.
+
+                switch (eingabe[1])
+                {
+
+                    case 0:
+
+                        Console.WriteLine("\nDas Programm wird nun beendent!\n");
+
+                        break;
+
+                    case 1:
+
+                        Console.Clear();
+
+                        Console.WriteLine("\n1.Taschenrechner");
+                        Console.Write("\nGeben Sie nun ein welches Programm Sie nutzen möchten: ");
+
+                        eingabe[2] = Convert.ToInt32(Console.ReadLine());
+
+                        switch (eingabe[2])
+                        {
+
+                            case 1:
+
+                                Console.Clear();
+
+                                Console.WriteLine("\n1.Addition\n2.Subtraktion\n3.Division\n4.Multiplikation\n5.Potenzrechnung");
+                                Console.Write("\nGeben Sie nun ein welches Programm Sie nutzen möchten: ");
+
+                                eingabe[3] = Convert.ToInt32(Console.ReadLine());
+
+                                // Ab hier werden Funktionen/Methoden aufgerufen
+                                switch (eingabe[3])
+                                {
+
+                                    case 1:
+
+                                        rechnungen.Addi();
+
+                                        break;
+
+                                    case 2:
+
+                                        rechnungen.Subtr();
+
+                                        break;
+
+                                    case 3:
+
+                                        rechnungen.Divi();
+
+                                        break;
+
+                                    case 4:
+
+                                        rechnungen.Mlti();
+
+                                        break;
+
+                                    case 5:
+
+                                        rechnungen.Ptz();
+
+                                        break;
+                                }
+
+                                break;
+
+                        }
+
+                        break;
+
+                }
+            }
+        }
+    }
+}
+
---
+++ rechnungen.cs
@@ -0,0 +1,138 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class rechnungen
+    {
+
+
+        public static void Addi()
+        {
+            double[] input = new double[2], erg = new double[1];
+
+            Console.Clear();
+
+            Console.WriteLine("\n");
+            Console.WriteLine("\t\tDer Additionsrechner\n");
+
+            Console.Write("Geben Sie ein wie oft Sie eine Zahl Addieren möchten: ");
+
+            input[0] = Convert.ToInt32(Console.ReadLine());
+
+            for (int x = 1; x <= input[0]; x++)
+            {
+
+                Console.Write("\nGeben Sie den {0}. Wert ein: ", x);
+                input[1] = Convert.ToDouble(Console.ReadLine());
+
+                erg[0] += input[1];
+            }
+
+            Console.WriteLine("\nDie Summe aller Zahlen lautet: {0}", erg[0]);
+
+        }
+
+        public static void Subtr()
+        {
+            double[] input = new double[3], erg = new double[1];
+
+            Console.Clear();
+
+            Console.WriteLine("\n");
+            Console.WriteLine("\t\tDer Subtrakionsrechner\n");
+
+            Console.Write("Geben Sie ein wie Sie eine Zahl Subtrahieren möchten: ");
+            input[0] = Convert.ToInt32(Console.ReadLine());
+
+            for (int x = 1; x <= input[0]; x++)
+            {
+
+                Console.Write("\nGeben Sie den {0}. Wert ein: ", x);
+                input[1] = Convert.ToDouble(Console.ReadLine());
+
+                erg[0] -= input[1];
+
+            }
+
+            Console.WriteLine("\nDie Summe aller Subtrahierten Zahlen lautet: {0}", erg[0]);
+
+        }
+
+        public static void Divi()
+        {
+
+            double[] input = new double[2], erg = new double[1];
+
+            Console.Clear();
+
+            Console.WriteLine("\n");
+            Console.WriteLine("\t\tDer Divisionsrechner");
+
+            Console.Write("Geben Sie den 1. Wert ein, den Sie Dividiren möchten: ");
+            input[4] = Convert.ToDouble(Console.ReadLine());
+
+            Console.Write("Geben Sie den 2. Wert ein, den Sie Dividiren möchten: ");
+            input[5] = Convert.ToDouble(Console.ReadLine());
+
+            erg[2] = input[4] / input[5];
+
+            Console.WriteLine("\nIhr Ergebnis lautet dank der Formel {0} / {1} = {2}\n", input[4], input[5], erg[2]);
+
+        }
+
+        public static void Mlti()
+        {
+            double[] input = new double[2], erg = new double[1];
+
+            Console.WriteLine("\n");
+            Console.WriteLine("\t\tDer Multiplikationsrechner");
+
+            Console.Write("Geben Sie den 1. Wert ein, den Sie Multiplizieren möchten: ");
+            input[0] = Convert.ToDouble(Console.ReadLine());
+
+            Console.Write("Geben Sie den 2. Wert ein, den Sie Multiplizieren möchten: ");
+            input[1] = Convert.ToDouble(Console.ReadLine());
+
+            erg[0] = input[1] * input[2];
+
+            Console.WriteLine("\nIhr Ergebnis lautet dank der Formel {0} * {1} = {2}\n", input[0], input[1], erg[0]);
+
+        }
+
+        public static void Ptz()
+        {
+            double[] input = new double[2], erg = new double[1];
+
+
+            Console.Clear();
+
+            Console.WriteLine("\n");
+            Console.WriteLine("\t\tDie Potenzrechung");
+
+            Console.Write("\nGeben Sie die Basis ein: ");
+            input[0] = Convert.ToDouble(Console.ReadLine());
+
+            Console.Write("\nGeben Sie den Exponenten ein: ");
+            input[1] = Convert.ToDouble(Console.ReadLine());
+
+            erg[0] = 1;
+
+            for (int x = 1; x <= input[1]; x++)
+            {
+
+                erg[0] *= input[0];
+
+                Console.WriteLine("Ihr {0}. Ergebnis lautet: {1}", x, erg[0]);
+
+            }
+
+
+        }
+
+    }
+}
+
