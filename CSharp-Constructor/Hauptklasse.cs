81089224b5d33578a8a88fc7c2ce2ff59e71e4a7
Trenxo2 <33938808+Trenxo2@users.noreply.github.com>
2019-03-04 15:14:51 +0100

    IHopeThatThisIsAConstructor

---
+++ CSharp-Constructor/Hauptklasse.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Uebungen
+{
+    class Hauptklasse
+    {
+        static void Main(string[] args)
+        {
+
+            int[] x = new int[2], input = new int[2];
+
+            Random zufall = new Random();
+
+            Console.Title = "Windows Konsolenanwendung";
+
+            Console.WriteLine("\t\t Windows Konsolenanwendung\n\n");
+
+            Console.WriteLine("\n1.Zufallsgenerator\n2.Nafinrn");
+            Console.Write("\nSuch Sie sich ein Programm aus(nur mit Zahlen): ");
+
+            input[0] = Convert.ToInt32(Console.ReadLine());
+
+            switch (input[0])
+            {
+
+                case 1:
+
+                    Programme.Zufallsgenerator();
+
+                    break;
+
+            }
+        }
+    }
+}
---
+++ CSharp-Constructor/Programme.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Uebungen
+{
+    class Programme
+    {
+
+        // Konfiguration
+
+        public int x, y;
+        public static int generiertezahl;
+
+        // Konsturktor
+
+        public Programme(int _x, int _y)
+        {
+
+            x = _x;
+            y = _y;
+
+            Random zufall = new Random();
+
+            generiertezahl = zufall.Next(x, y);
+
+        }
+
+        public static int getGenerierteZahl()
+        {
+            Console.WriteLine("\n Die Zufällig Generierte Zahl lautet: {0}", generiertezahl);
+
+            return generiertezahl;
+        }
+
+        public static void Zufallsgenerator()
+        {
+
+            int[] x = new int[3];
+
+            Random zufall = new Random();
+
+            Console.Clear();
+
+            Console.WriteLine("\t\tZufallsgenerator");
+
+            Console.Write("\nGeben Sie die 1. Zahl ein: ");
+            x[0] = Convert.ToInt32(Console.ReadLine());
+
+            Console.Write("\nGeben Sie die 2. Zahl ein: ");
+            x[1] = Convert.ToInt32(Console.ReadLine());
+
+            Programme genZahl = new Programme(x[0], x[1]);
+
+            Console.Clear();
+
+            getGenerierteZahl();
+
+        }
+
+    }
+}
