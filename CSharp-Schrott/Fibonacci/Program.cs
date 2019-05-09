6b34389ca179368859c020cc93d5edadc3694f40
Trenxo2 <33938808+Trenxo2@users.noreply.github.com>
2019-05-09 17:36:29 +0200

    ripme

---
+++ CSharp-Schrott/Fibonacci/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace F_Folge
+{
+    class Program
+    {
+
+        private static void Calculation(int x)
+        {
+            int erste = 0, zweite = 1, erg = 0;
+
+            for (int i = 1; i <= x; i++)
+            {
+                Console.WriteLine("{0}. Fibonacci Zahl: {1}\n", i, erg);
+
+                erste += 1;
+                zweite += 1;
+
+                erste = zweite;
+                zweite = erg;
+                
+                erg = erste + zweite - 1;
+            }
+        }
+
+        static void Main(string[] args)
+        {
+            Calculation(15);
+        }
+    }
+}
